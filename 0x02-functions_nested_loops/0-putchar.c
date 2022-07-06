062686b3b755c80e2b3fa37116f445adc5d857b0
Oluwafemi <ogundarearnold@gmail.com>
2022-07-06 05:41:19 +0000

    still not working

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
+
 /**
 * main - prints putchar
 * Return: 0 (Success)
 */
 int main(void)
 {
-
 	 putchar('');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
