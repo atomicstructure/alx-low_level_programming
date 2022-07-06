1c3f42805124bfa50b2656f9a6f6c881042db47d
Oluwafemi <ogundarearnold@gmail.com>
2022-07-06 05:23:52 +0000

    trying it the hard way

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,18 +3,17 @@
 * main - prints putchar
 * Return: 0 (Success)
 */
-  
 int main(void)
 {
-          putchar('');
-          _putchar('p');
-          _putchar('u');
-          _putchar('t');
-          _putchar('c');
-          _putchar('h');
-          _putchar('a');
-          _putchar('r');
-          _putchar('\n');
- 
-          return (0);
- }
+
+	 putchar('');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
