1cda7286a6908089c148ecffd8a4b2143231304d
oluwafemi <ogundarearnold@gmail.com>
2022-07-06 02:04:23 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,13 @@
 * print_alphabet -> prints the lowercase alphabets
 *
 * Return: 0 (Success
-*/)
+*/
 void print_alphabet(void)
 {
 	int j;
 	for (j = 'a'; j <= 'z'; j++)
 	{
-	_putchar(j);
+		_putchar(j);
 	}
-	_putchar('\n');
+		_putchar('\n');
 }
