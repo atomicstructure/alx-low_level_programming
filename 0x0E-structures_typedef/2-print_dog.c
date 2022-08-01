1e48573d9cf341b1b2aded89284ae280a1fb75dd
Ogundare Olusesi Oluwafemi <86707117+atomicstructure@users.noreply.github.com>
2022-08-01 09:39:19 -0700

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,4 +15,5 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
 		printf("Age: %f\n", (d->age) ? d->age : 0);
 		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
-	}}
+	}
+}
